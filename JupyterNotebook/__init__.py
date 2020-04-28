fcda9fbf1dd30adb2ff98a21f7e54dbe05bd8b8a
Gabriel Vieira <48156370+gabrielvieiraf@users.noreply.github.com>
2020-04-28 17:00:39 -0300

    Create __init__.py

---
+++ JupyterNotebook/__init__.py
@@ -0,0 +1,21 @@
+import os
+
+# Packagers: Modifique a próxima linha se você armazenar os arquivos de modelo do notebook em outro local
+DEFAULT_STATIC_FILES_PATH = os.path.join(os.path.dirname(__file__), "static")
+
+# Inclua notebook / e notebook / modelos /. Isso torna possível que os usuários substituam um modelo por
+# um arquivo herdado daquele template.
+#
+# Por exemplo, se você deseja substituir um bloco específico de notebook.html, pode
+# pode criar um arquivo chamado notebook.html que herda de templates / notebook.html,
+# e o último será resolvido corretamente para a base implementação.
+
+DEFAULT_TEMPLATE_PATH_LIST = [
+    os.path.dirname(__file__),
+    os.path.join(os.path.dirname(__file__), "templates"),
+]
+
+del os
+
+from .nbextensions import install_nbextension
+from ._version import version_info, __version__
